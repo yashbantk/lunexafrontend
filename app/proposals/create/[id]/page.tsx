fb40a52ecb8056c6678118f0f0b16017b5fe0026
yashbantk <yaarora1234@gmail.com>
2025-09-28 13:32:10 +0530

    remvoe pdf import and everything

--- app/proposals/create/[id]/page.tsx
+++ app/proposals/create/[id]/page.tsx
@@ -1156,7 +1156,6 @@ export default function CreateProposalPage() {
               <PriceSummary
                 proposal={proposal}
                 onSaveProposal={() => saveProposal(proposal)}
-                onExportPDF={() => window.print()}
                 onPreview={() => console.log('Preview proposal')}
               />
             </motion.div>
